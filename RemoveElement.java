950b5efa694cff82b04f547989f233f5c756d947
muthuselvamump <muthuselvamump@gmail.com>
2025-01-23 20:49:58 +0530

    27.RemoveElement

--- RemoveElement.java
+++ RemoveElement.java
@@ -1,3 +1,18 @@
+class Solution27 {
+    public int removeElement(int[] nums, int val) {
+        int k=0;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(val!=nums[i]){
+                 k++;
+                nums[count++]=nums[i];
+            }
+
+        }
+        return k;
+        
+    }
+}
 public class RemoveElement{
 public static void main(String[] args) {
     
