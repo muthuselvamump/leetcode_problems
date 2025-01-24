aa8c25f35cbb3fc951dded8cbc15396617a08f14
muthuselvamump <muthuselvamump@gmail.com>
2025-01-24 20:45:02 +0530

    RemoveDuplicatesfromSortedArray26

---
+++ RemoveDuplicatesfromSortedArray26.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution26{
+    public int removeDuplicates(int[] nums) {
+        int k=0;
+        int count=0;
+    HashSet<Integer> h=new HashSet<>();
+    for(int i=0;i<nums.length;i++){
+        h.add(nums[i]);
+    }
+   
+    return k;
+    }
+}
+public class RemoveDuplicatesfromSortedArray26 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
