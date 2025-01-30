d0445ffd964a41ba96c25600c4fbf27ed26ca534
muthuselvamump <muthuselvamump@gmail.com>
2025-01-30 20:52:01 +0530

    88_Merge Sorted Array

--- 88_Merge Sorted Array.java
+++ 88_Merge Sorted Array.java
@@ -1,38 +1,30 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int arr[]=new int[m+n];
-        int narr=m+n;
         int count=0;
         int countk=0;
-        int arrc=0;
-        for(int i=0;i<m+n;i++){
-            if(count<nums1.length && countk<nums2.length){
-                if(nums1[count]<nums2[countk]){
-                    if(nums1[count]!=0){
-                    arr[arrc++]=nums1[count++];
-                    }
-                    else{
-                        arr[arrc++]=nums2[countk++];
-                    }
+        int arr[]=new int[m+n];
+        int ac=0;
+        while(count<m && countk<n){
+            if(nums1[count]<nums2[countk]){
+                arr[ac++]=nums1[count++];
+            }
+            else{
+                arr[ac++]=nums2[countk++];
+            }
+        }
+            while(count<m || countk <n){
+                if(countk<n){
+                    arr[ac++]=nums2[countk++];
                 }
                 else{
-                    if(nums2[countk]!=0){
-                    arr[arrc++]=nums2[countk++];
-                    }
-                    else{
-                        arr[arrc++]=nums1[count++];
-                    }
+                    arr[ac++]=nums1[count++];
                 }
-
             }
-          
+            for(int i=0;i<arr.length;i++){
+                nums1[i]=arr[i];
+            }
 
-        }
-        if(n!=0){
-        for(int i=0;i<arr.length;i++){
-            nums1[i]=arr[i];
-        }
-        }
+
 
         
     }
