85b0786e250d76dcd38d4dde125202a8a70b011a
muthuselvamump <muthuselvamump@gmail.com>
2025-01-30 21:42:35 +0530

    MajorityElement169

--- MajorityElement169.java
+++ MajorityElement169.java
@@ -1,17 +1,18 @@
-class Solution169 {
+class Solution169{
     public int majorityElement(int[] nums) {
         int max=0;
         int count=0;
-        int prv=0;
+        int prv=nums.length/2;
         for(int i=0;i<nums.length;i++){
             for(int j=0;j<nums.length;j++){
                 if(nums[i]==nums[j]){
                     count++;
                 }
             }
+
             if(count>prv){
                 max=nums[i];
-                prv=count;
+                return max;
             }
             count=0;
 
