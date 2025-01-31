cfcf7a3cba0b0d1d00d78832e246e79de81f63c1
muthuselvamump <muthuselvamump@gmail.com>
2025-01-31 20:58:34 +0530

    BestTimetoBuyandSellStock

--- BestTimetoBuyandSellStock.java
+++ BestTimetoBuyandSellStock.java
@@ -1,34 +1,19 @@
-class Solution121 {
+class Solution {
     public int maxProfit(int[] prices) {
         int min=Integer.MAX_VALUE;
-        int max=0;
-        int index=0;
-        for(int i=0;i<1;i++){
-            for(int j=0;j<prices.length;j++){
-                if(prices[j]<min){
-                    min=prices[j];
-                    index=j;
-                }
+        int profit=0;
+        for(int i=0;i<prices.length;i++){
+            if(min>prices[i]){
+                min=prices[i];
+            }
+            else if(prices[i]-min>profit){
+                profit=prices[i]-min;
             }
         }
-        for(int i=0;i<1;i++){
-        for(int k=index;k<prices.length;k++){
-            if(max<prices[k]){
-                max=prices[k];
-            }
-        }
-        }
-        if(min>max){
-            return 0;
-        }
-        else{
-            return max-min;
-        }
-
+        
         
     }
-}
-        
+}       
 public class BestTimetoBuyandSellStock {
     public static void main(String []args){
 
