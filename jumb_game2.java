fbfa09e679031cb0e93ce45ca64b9ee77df0d5ea
muthuselvamump <muthuselvamump@gmail.com>
2025-02-05 20:44:23 +0530

    jumb_game2

--- jumb_game2.java
+++ jumb_game2.java
@@ -1,6 +1,30 @@
+class Solution45 {
+    public int jump(int[] nums) {
+        if(nums.length==1){
+            return 0;
+        }
+        int cover=0;
+        int lastintex=0;
+        int jumb=0;
+        for(int i=0;i<nums.length;i++){
+            if(cover<nums[i]+i){
+                cover=nums[i]+i;
+            }
+            if(lastintex==i){
+                lastintex=cover;
+                jumb++;
+            
+            if(cover>=nums.length-1){
+                return jumb;
+            }
+        }
+        }
+        return jumb;
+    }
+}
 public class jumb_game2 {
     public static void  main(String args[]){
-        
+
     }
     
 }
