0a85687fe3f8eaac9c7d5bd8ccbec0b8f33c5bb9
muthuselvamump <muthuselvamump@gmail.com>
2025-02-17 23:13:54 +0530

    Palindrome_Number

--- Palindrome_Number.java
+++ Palindrome_Number.java
@@ -1,3 +1,15 @@
+class Solution9 {
+    public boolean isPalindrome(int x) {
+        String str1=Integer.toString(x);
+        String str2="";
+        for(int i=str1.length()-1;i>=0;i--){
+            str2+=str1.charAt(i);
+        }
+        System.out.println(str1 +" " +str2);
+        return str1.equals(str2);
+        
+    }
+}
 public class Palindrome_Number {
     public static void main(String[] args) {
         
