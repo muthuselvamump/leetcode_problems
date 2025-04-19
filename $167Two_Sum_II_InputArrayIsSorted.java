bfa9eed66cd3bc593d46d8a6984662a8c4961489
muthuselvamump <muthuselvamump@gmail.com>
2025-04-19 21:56:53 +0530

    167.Two Sum II - Input Array Is Sorted

--- $167Two_Sum_II_InputArrayIsSorted.java
+++ $167Two_Sum_II_InputArrayIsSorted.java
@@ -1,3 +1,25 @@
+class Solution167{
+    public int[] twoSum(int[] numbers, int target) {
+        for(int i=0;i<numbers.length-1;i++){
+            int left=i;
+            int right=numbers.length-1;
+            while(left<right){
+                int sum=numbers[left]+numbers[right];
+                if(sum==target){
+                    return new int[]{left+1,right+1};
+                }
+                else if(sum<target){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            }
+        }
+        return new int[]{-1,-1};
+        
+    }
+}
 public class $167Two_Sum_II_InputArrayIsSorted{
     public static void main(String args[]){
 
