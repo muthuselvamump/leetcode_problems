492adbbdc416cf275193a58db5bb714adc5d4b3a
muthuselvamump <muthuselvamump@gmail.com>
2025-04-21 16:35:50 +0530

    Two Sum

--- Two_Sum.java
+++ Two_Sum.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 class Solution1_Brute_Force {
     public int[] twoSum(int[] nums, int target) {
         int arr[]=new int[2];
@@ -10,6 +12,20 @@ class Solution1_Brute_Force {
             }
         }
         return arr;
+    }
+    class Solution_Optimal_solution {
+        public int[] twoSum(int[] nums, int target) {
+            HashMap<Integer,Integer> n=new HashMap();
+    
+            for(int i=0;i<nums.length;i++){
+                int sum=target-nums[i];
+                if(n.containsKey(sum)){
+                    return new int[]{i,n.get(sum)};
+                }
+                n.put(nums[i],i);
+            }
+            return new int[]{-1,-1};
+        }
     }
 }
 public class Two_Sum {
