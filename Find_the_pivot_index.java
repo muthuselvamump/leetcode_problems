e4ab135c41b9325b647b9667a2ce6b654ae0cd32
muthuselvamump <muthuselvamump@gmail.com>
2025-04-22 20:19:05 +0530

    Find_the_pivot_index

--- Find_the_pivot_index.java
+++ Find_the_pivot_index.java
@@ -1,6 +1,23 @@
+class Solutionprefixsum {
+    public int pivotIndex(int[] nums) {
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        int left=0;
+        for(int i=0;i<nums.length;i++){
+            if(left==sum-left-nums[i]){
+                return i;
+            }
+            left+=nums[i];
+        }
+        return -1;
+        
+    }
+}
 public class Find_the_pivot_index {
     public static void main(String args[]){
-        
+
     }
     
 }
