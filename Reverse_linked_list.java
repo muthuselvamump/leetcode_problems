9406f6fb07b433188e050891e3b6bb0b5b6e0e85
muthuselvamump <muthuselvamump@gmail.com>
2025-04-23 19:45:25 +0530

    Reverse_linked_list

--- Reverse_linked_list.java
+++ Reverse_linked_list.java
@@ -1,6 +1,25 @@
+class Solution_inplaceorder {
+    public ListNode reverseList(ListNode head) {
+
+
+        ListNode prev=null;
+        ListNode current=head;
+
+        while(current!=null){
+            ListNode temp=current.next;
+            current.next=prev;
+            prev=current;
+            current=temp;
+        }
+        return prev;
+
+
+        
+    }
+}
 public class Reverse_linked_list {
     public static void main(String args[]){
-        
+
     }
     
 }
