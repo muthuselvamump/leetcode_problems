a22e852df21e4759f96f411a5f60fb7ef1b75476
muthuselvamump <muthuselvamump@gmail.com>
2025-04-27 14:21:46 +0530

    Kth_Largest_Element_in_an_Array

--- Kth_Largest_Element_in_an_Array.java
+++ Kth_Largest_Element_in_an_Array.java
@@ -1,3 +1,19 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class SolutionKthlargestpattern{
+    public int findKthLargest(int[] nums, int k) {
+        Queue<Integer> q=new PriorityQueue<>();
+        for(int i=0;i<nums.length;i++){
+            q.add(nums[i]);
+            if(q.size()>k){
+                q.poll();
+            }
+        }
+        return q.poll();
+        
+    }
+}
 public class Kth_Largest_Element_in_an_Array {
     public static void main(String[] args) {
         
