6a7589a305b68028ebfb8d849238f543341d5878
muthuselvamump <muthuselvamump@gmail.com>
2025-04-28 20:10:45 +0530

    Find_Smallest_Letter_Greater_Than_Target

--- Find_Smallest_Letter_Greater_Than_Target.java
+++ Find_Smallest_Letter_Greater_Than_Target.java
@@ -1,3 +1,25 @@
+class Solution_BinarysearchNGL {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start=0;
+        int end=letters.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(letters[mid]<=target){
+                start=mid+1;
+                
+            }
+            else{
+                end=mid-1;
+                
+            }
+        }
+        if(start==letters.length){
+            return letters[0];
+        }
+        return letters[start];
+        
+    }
+}
 public class Find_Smallest_Letter_Greater_Than_Target {
     public static void main(String[] args) {
         
