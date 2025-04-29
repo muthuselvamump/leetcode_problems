ad725b2d0d06a824d676bfaebb99cc9580db0f7f
muthuselvamump <muthuselvamump@gmail.com>
2025-04-29 23:17:56 +0530

    searchinmatrix

--- searchinmatrix.java
+++ searchinmatrix.java
@@ -1,3 +1,21 @@
+class Searchinmatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int r=0;
+        int c=matrix[0].length-1;
+        while(r<matrix.length && c>=0){
+            if(matrix[r][c]==target){
+                return true;
+            }
+            else if(matrix[r][c]<target){
+                r++;
+            }
+            else{
+                c--;
+            }
+        }
+        return false;
+    }
+}
 public class searchinmatrix {
     public static void main(String[] args) {
         
