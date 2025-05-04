c4eccf6fde507af5aa95384743ff22b32f5dec71
muthuselvamump <muthuselvamump@gmail.com>
2025-05-04 17:45:03 +0530

    MissingNumber

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,3 +1,30 @@
+class SolutionCyclicsortalgo {
+    public int missingNumber(int[] nums) {
+        int i=0;
+        while(i<nums.length){
+            if(nums[i]==i){
+                i++;
+            }
+            else{
+                if(nums[i]>=nums.length){
+                    i++;
+                }
+                else{
+                int temp=nums[i];
+                nums[i]=nums[temp];
+                nums[temp]=temp;}
+            }
+        }
+
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=j){
+                return j;
+            }
+        }
+        //System.out.println(Arrays.toString(nums));
+        return nums.length;
+    }
+}
 public class MissingNumber {
     public static void main(String[] args) {
         
