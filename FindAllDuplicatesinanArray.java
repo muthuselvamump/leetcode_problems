8fb61834f1f2bf6341b2a90f94deaf75baaf4c25
muthuselvamump <muthuselvamump@gmail.com>
2025-05-05 23:34:50 +0530

    FindAllDuplicatesinanArray

--- FindAllDuplicatesinanArray.java
+++ FindAllDuplicatesinanArray.java
@@ -1,3 +1,34 @@
+import java.util.*;
+
+class SolutionCyclicsortalgofd {
+    public List<Integer> findDuplicates(int[] nums) {
+         List<Integer> ans=new ArrayList<>();
+        int i=0;
+        while(i<nums.length){
+            if((nums[i]-1)==i){
+                i++;
+            }
+            else{
+                int temp=nums[i];
+                if(nums[i]==nums[temp-1]){
+                    i++;
+                }
+                else{
+                nums[i]=nums[temp-1];
+                nums[temp-1]=temp;}
+            }
+        }
+
+        for(int j=0;j<nums.length;j++){
+            if((nums[j]-1)!=j){
+                ans.add(nums[j]);
+            }
+        }
+        
+        return ans;
+        
+    }
+}
 public class FindAllDuplicatesinanArray {
     public static void main(String[] args) {
         
