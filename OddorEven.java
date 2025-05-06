4dc27a3966af3d61db07a4f2350b49d335f7ea4c
muthuselvamump <muthuselvamump@gmail.com>
2025-05-06 20:28:16 +0530

    Odd or Even

--- OddorEven.java
+++ OddorEven.java
@@ -1,3 +1,12 @@
+class Solutionusingbitwiseoperator {
+    static boolean isEven(int n) {
+       if((n&1)==1){
+           return false;
+       }
+        
+        return true;
+    }
+}
 public class OddorEven {
     public static void main(String[] args) {
         
