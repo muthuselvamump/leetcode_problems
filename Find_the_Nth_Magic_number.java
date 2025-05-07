d35e36913f33b02091c8a6b3d89ee27786637852
muthuselvamump <muthuselvamump@gmail.com>
2025-05-07 15:49:16 +0530

    Find_the_Nth_Magic_number

--- Find_the_Nth_Magic_number.java
+++ Find_the_Nth_Magic_number.java
@@ -1,5 +1,20 @@
 public class Find_the_Nth_Magic_number{
     public static void main(String[] args) {
+        System.out.println(magic(5));
+        
+    }
+    public static int magic(int n) {
+
+        int pow=5;
+        int ans=0;
+
+        while(n>0){
+            int last=n&1;
+            n=n>>1;
+            ans+=last*pow;
+            pow=pow*5;
+        }
+        return ans;
         
     }
 }
