958811822a7178ea67818b6fe224abb0a0e5e5e4
muthuselvamump <muthuselvamump@gmail.com>
2025-05-08 22:09:45 +0530

    SingleNumberII

--- SingleNumberII.java
+++ SingleNumberII.java
@@ -1,3 +1,16 @@
+class Solution_bitvise_operator {
+    public int singleNumber(int[] nums) {
+
+        int ones=0;
+        int twos=0;
+        for(int ele:nums){
+            ones=(ele^ones)&(~twos);
+            twos=(ele^twos)&(~ones);
+        }
+        return ones;
+        
+    }
+}
 public class SingleNumberII {
     public static void main(String[] args) {
         
