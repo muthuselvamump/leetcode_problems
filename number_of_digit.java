847ec704f124f3118cfdb11c606718e7c1cb4bd4
muthuselvamump <muthuselvamump@gmail.com>
2025-05-10 11:59:47 +0530

    number_of_digit

--- number_of_digit.java
+++ number_of_digit.java
@@ -1,6 +1,11 @@
 public class number_of_digit {
     public static void main(String[] args) {
+        System.out.println(countofdigit(255, 10));
+        System.out.println(countofdigit(2, 2));
+    }
+    public static int countofdigit(int n,int b) {
+
+        return (int)((Math.log(n)/Math.log(b))+1);
         
     }
-    
 }
