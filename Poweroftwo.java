ef679ed6d1bf8bacece85595933d2f90953ab49a
muthuselvamump <muthuselvamump@gmail.com>
2025-05-13 20:34:35 +0530

    Poweroftwo

--- Poweroftwo.java
+++ Poweroftwo.java
@@ -1,8 +1,12 @@
 public class Poweroftwo {
     public static void main(String[] args) {
+        System.out.println(powertwo(5));
+        System.out.println(powertwo(16));
         
     }
-    public static void powertwo(int n) {
+    public static boolean powertwo(int n) {
+
+        return n>0 && (n&(n-1))==0;
         
     }
     
