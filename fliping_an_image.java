8517ee72a0bff1d675b879efd0618f14e79dbda0
muthuselvamump <muthuselvamump@gmail.com>
2025-05-14 19:15:57 +0530

    fliping_an_image

---
+++ fliping_an_image.java
@@ -0,0 +1,20 @@
+class Solution832 {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for(int i=0;i<image.length;i++){
+            for(int j=0;j<((image[i].length+1)/2);j++){
+                int temp=image[i][j]^1;
+                image[i][j]=image[i][image[i].length-j-1]^1;
+                image[i][image[i].length-j-1]=temp;
+
+
+            }
+        }
+        return image;
+    }
+}
+public class fliping_an_image {
+    public static void main(String[] args) {
+        
+    }
+    
+}
