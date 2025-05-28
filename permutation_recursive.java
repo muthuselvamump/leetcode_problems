c9c6c6015eb1639769ad7d181460986936b3f7b7
muthuselvamump <muthuselvamump@gmail.com>
2025-05-28 20:10:04 +0530

    permutation_recursive

--- permutation_recursive.java
+++ permutation_recursive.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class permutation_recursive {
     public static void main(String[] args) {
         permrecu("", "abc");
+        int []arr={1,2,3};
+        List<Integer> l1=Arrays.stream(arr).boxed().toList();
+        System.out.println();
+        System.out.println(permuter(new ArrayList<>(),l1,new ArrayList<List<Integer>>()));
+
     }
     public static void permrecu(String p,String up) {
         if(up.isEmpty()){
@@ -14,5 +23,21 @@ public class permutation_recursive {
             permrecu(f+ch+s, up.substring(1));
         }
     }
-    
+
+    public static List<List<Integer>> permuter(List<Integer> p,List<Integer> up,List<List<Integer>> list) {
+        if(up.size()==0){
+            list.add(p);
+            return list;
+        }
+        
+        for(int i=0;i<p.size()+1;i++){
+        ArrayList<Integer> n=new ArrayList<>(p);
+        ArrayList<Integer> nq=new ArrayList<>(up);
+            n.add(i,nq.remove(0));
+            permuter(n,nq,list);
+        }
+        return list;
+
+       
+    }
 }
