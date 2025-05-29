ebd3bbe2fca1f6d49d23b76787c947324e25d943
muthuselvamump <muthuselvamump@gmail.com>
2025-05-29 20:04:07 +0530

    dice_roll

--- dice_roll.java
+++ dice_roll.java
@@ -1,6 +1,8 @@
 public class dice_roll {
     public static void main(String[] args) {
         dicer("", 4);
+        System.out.println();
+        diceown("", 4, new int[]{1,3,5,7});
     }
     public static void dicer(String p,int up) {
         if(up==0){
@@ -9,6 +11,19 @@ public class dice_roll {
         }
         for(int i=1;i<=6 && i<=up;i++){
             dicer(p+i, up-i);
+        }
+        
+    }
+    public static void diceown(String p,int up,int []arr) {
+        if(up==0){
+            System.out.print(p+" ");
+            return;
+        }
+
+        for(int i:arr){
+            if(up>=i){
+                diceown(p+i, up-i, arr);
+            }
         }
         
     }
