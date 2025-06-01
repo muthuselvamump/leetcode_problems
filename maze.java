e59483ca579f31c4e2ed58ac2fe0bcb54d5553a3
muthuselvamump <muthuselvamump@gmail.com>
2025-06-01 23:57:15 +0530

    maze

--- maze.java
+++ maze.java
@@ -2,5 +2,12 @@ public class maze {
     public static void main(String[] args) {
         
     }
+    public static int mazedownadn rightcount(String p,int r,int c) {
+        if(r==0 & c==0){
+            return 1;
+        }
+
+        
+    }
     
 }
