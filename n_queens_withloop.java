852bcf8d6fc8bd3438ce0c201d06cc31db73959c
muthuselvamump <muthuselvamump@gmail.com>
2025-06-19 21:45:06 +0530

    n_queens_withloop

---
+++ n_queens_withloop.java
@@ -0,0 +1,9 @@
+public class n_queens_withloop {
+    public static void main(String[] args) {
+        
+    }
+    public static void nqueensloop(boolean board[][],int row) {
+        
+    }
+    
+}
