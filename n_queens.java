c0808e769f3a86ff7af27328d4c923a9213a1215
muthuselvamump <muthuselvamump@gmail.com>
2025-06-19 21:42:44 +0530

    n_queens

--- n_queens.java
+++ n_queens.java
@@ -2,17 +2,28 @@ import java.util.Arrays;
 
 public class n_queens {
     public static void main(String[] args) {
+       // System.out.println(Arrays.deepToString(new int[4][4]));
+        nqueens(new boolean[4][4], 0, 0);
         
     }
     public static void nqueens(boolean board[][],int r,int c) {
 
-        if(r>=board.length){
-            Arrays.deepToString(board);
+        if(r==board.length){
+           System.out.println(Arrays.deepToString(board));
+            System.out.println();
+            return;
+        }
+        if(c>=board[0].length){
             return;
         }
         if(isSafe(board,r,c)){
+            board[r][c]=true;
+            nqueens(board, r+1, 0);
+            board[r][c]=false;
             
         }
+        nqueens(board, r, c+1);
+
         
     }
     private static boolean isSafe(boolean board[][],int r,int c) {
