0dc842ba3c465a6a7ea4a975b4dddfff9e9a9312
muthuselvamump <muthuselvamump@gmail.com>
2025-06-23 22:47:17 +0530

    n_queens_withloop

--- n_queens_withloop.java
+++ n_queens_withloop.java
@@ -1,8 +1,14 @@
+import java.util.Arrays;
+
 public class n_queens_withloop {
     public static void main(String[] args) {
         
     }
     public static void nqueensloop(boolean board[][],int row) {
+        if(board.length==row){
+            System.out.println(Arrays.deepToString(board));
+            return;
+        }
         
     }
     
