8f839fc17444bb73d4458fc104bdf88c4e0a93be
muthuselvamump <muthuselvamump@gmail.com>
2025-07-06 18:59:55 +0530

    n_knightts.java

--- n_knightts.java
+++ n_knightts.java
@@ -1,9 +1,43 @@
+import java.util.Arrays;
+
 public class n_knightts {
     public static void main(String[] args) {
         
     }
-    public static void main(String[] args) {
+    public static void nknightts(boolean board[][],int kt) {
+        if(kt==0){
+            System.out.println(Arrays.deepToString(board));
+            return;
+        }
         
     }
-    
+    public static boolean isSafe(boolean board[][],int row,int col){
+        //top right case
+        if(isValid(board,row-2,col+1)){
+            
+        }
+        //top left case
+        if(isValid(board,row-2,col-1)){
+            
+        }
+
+        //left top case
+        if(isValid(board,row-1,col-2)){
+            
+        }
+
+        //right top case
+        if(isValid(board,row-2,col+1)){
+            
+        }
+
+
+
+    }
+    private static boolean isValid(boolean[][] board, int row, int col) {
+        if(0<){
+
+        }
+
+    }
 }
