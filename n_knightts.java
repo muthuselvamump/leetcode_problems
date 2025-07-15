3f5654554fc26e9ba8d8fb135b1613fade525575
muthuselvamump <muthuselvamump@gmail.com>
2025-07-15 21:06:10 +0530

    n_knightts

--- n_knightts.java
+++ n_knightts.java
@@ -2,13 +2,27 @@ import java.util.Arrays;
 
 public class n_knightts {
     public static void main(String[] args) {
+        nknightts(new boolean[4][4], 4, 0, 0);
         
     }
-    public static void nknightts(boolean board[][],int kt) {
+    public static void nknightts(boolean board[][],int kt,int row,int col) {
         if(kt==0){
             System.out.println(Arrays.deepToString(board));
             return;
         }
+        if(col>=board[0].length){
+            nknightts(board, kt, row+1, 0);
+            return;
+        }
+        if(row>=board.length){
+            return;
+        }
+        if(isSafe(board,row,col)){
+            board[row][col]=true;
+            nknightts(board, kt-1, row, col+1);
+            board[row][col]=false;
+        }
+        nknightts(board, kt, row, col+1);
         
     }
     public static boolean isSafe(boolean board[][],int row,int col){
