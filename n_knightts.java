1442d18334592941b2261b53587dece11d0ac915
muthuselvamump <muthuselvamump@gmail.com>
2025-07-15 20:53:26 +0530

    n_knightts

--- n_knightts.java
+++ n_knightts.java
@@ -14,30 +14,45 @@ public class n_knightts {
     public static boolean isSafe(boolean board[][],int row,int col){
         //top right case
         if(isValid(board,row-2,col+1)){
+            if(board[row-2][col+1]){
+                return false;
+            }
             
         }
         //top left case
         if(isValid(board,row-2,col-1)){
+            if(board[row-2][col-1]){
+                return false;
+            }
             
         }
 
         //left top case
         if(isValid(board,row-1,col-2)){
+            if(board[row-1][col-2]){
+                return false;
+            }
             
         }
 
         //right top case
-        if(isValid(board,row-2,col+1)){
+        if(isValid(board,row-1,col+2)){
+            if(board[row-1][col+2]){
+                return false;
+            }
             
         }
+        return true;
 
 
 
     }
     private static boolean isValid(boolean[][] board, int row, int col) {
-        if(0<){
+        if(0>row || col<0 || col>=board[0].length || row>=board.length){
+            return false;
 
         }
+        return true;
 
     }
 }
