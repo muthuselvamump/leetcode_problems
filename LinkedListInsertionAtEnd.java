3fe726d769d0e865a2cc26e85dd1348064e8aaea
muthuselvamump <muthuselvamump@gmail.com>
2025-07-30 23:02:46 +0530

    LinkedListInsertionAtEnd

--- LinkedListInsertionAtEnd.java
+++ LinkedListInsertionAtEnd.java
@@ -1,3 +1,34 @@
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+class SolutionGFGinserend {
+    public Node insertAtEnd(Node head, int x) {
+        // code here
+        if(head==null){
+            return new Node(x);
+        }
+        inserting(head,x);
+        return head;
+        
+    }
+    public void inserting(Node temp,int x){
+        if(temp.next==null){
+            Node t=new Node(x);
+           // t.data=x;
+            temp.next=t;
+            return;
+        }
+        inserting(temp.next,x);
+    }
+}
 public class LinkedListInsertionAtEnd {
     
 }
