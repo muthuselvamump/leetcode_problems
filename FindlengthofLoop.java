4430e0df92bb7f12c65916075dcbcba04692defc
muthuselvamump <muthuselvamump@gmail.com>
2025-07-31 22:19:24 +0530

    FindlengthofLoop

--- FindlengthofLoop.java
+++ FindlengthofLoop.java
@@ -1,3 +1,38 @@
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+*/
+
+class SolutionGFGFLL{
+    // Function to find the length of a loop in the linked list.
+    public int countNodesinLoop(Node head) {
+        Node fast=head;
+        Node slow=head;
+        do{
+            if(fast==null || fast.next==null){
+                return 0;
+            }
+            fast=fast.next.next;
+            slow=slow.next;
+            
+        }
+        while(fast!=slow);
+        
+        int count=0;
+        do{
+            slow=slow.next;
+            count++;
+        }
+        while(fast!=slow);
+        return count;
+        
+        // code here.
+    }
+}
 public class FindlengthofLoop{
-    
+
 }
