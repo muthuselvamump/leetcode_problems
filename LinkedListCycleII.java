4bba95c34848fd79eaad7c56a358374618f67f7b
muthuselvamump <muthuselvamump@gmail.com>
2025-08-07 22:24:32 +0530

    LinkedListCycleII

--- LinkedListCycleII.java
+++ LinkedListCycleII.java
@@ -1,3 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution142 {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+         do{
+            if(fast==null || fast.next==null){
+                return null;
+            }
+            fast=fast.next.next;
+            slow=slow.next;
+            
+        }
+        while(fast!=slow);
+        
+        fast=head;
+        
+        while(fast!=slow){
+            fast=fast.next;
+            slow=slow.next;
+        }
+        return fast;
+    }
+}
 public class LinkedListCycleII {
     public static void main(String[] args) {
         
