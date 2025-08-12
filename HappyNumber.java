01023ac1cdce687277405a3ea574d9d768090815
muthuselvamump <muthuselvamump@gmail.com>
2025-08-12 23:08:04 +0530

    HappyNumber

--- HappyNumber.java
+++ HappyNumber.java
@@ -1,3 +1,28 @@
+class Solution202 {
+    public boolean isHappy(int n) {
+        int fast=n;
+        int slow=n;
+        do{
+            slow=squaroot(slow);
+            fast=squaroot(squaroot(fast));
+            if(fast==1){
+                return true;
+            }
+        }
+        while(slow!=fast);
+        return false;
+    }
+    public int squaroot(int n){
+        int ans=0;
+        while(n>0){
+            int nu=n%10;
+            ans+=nu*nu;
+            n=n/10;
+        }
+        return ans;
+
+    }
+}
 public class HappyNumber {
     public static void main(String[] args) {
         
