1def12b67a64cf68b5bd3d2590c038453ba58c6a
muthuselvamump <muthuselvamump@gmail.com>
2025-08-18 22:46:51 +0530

    level_order_successor_of_a_node.java

--- level_order_successor_of_a_node.java
+++ level_order_successor_of_a_node.java
@@ -1,7 +1,38 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+import javax.swing.tree.TreeNode;
 
 public class level_order_successor_of_a_node {
     public static void main(String[] args) {
         
+    }
+    public static int level_orde(TreeNode root,int sval){
+        Queue<TreeNode> s=new LinkedList<>();
+        if(root==null){
+            return -1;
+        }
+        s.offer(root);
+        while(!s.isEmpty()){
+            TreeNode temp=s.poll();
+            if(temp.left!=null){
+                s.offer(temp.left);
+            }
+            if(temp.right!=null){
+                s.offer(temp.right);
+
+            }
+            if(temp.val==sval){
+                break;
+            }
+        }
+        int answer=-1;
+        if(!s.isEmpty()){
+        TreeNode ans=s.poll();
+        answer=ans.val;
+    }
+        return answer;
+
     }
     
 
