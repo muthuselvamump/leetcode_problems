3a312f81b10d3870d69e928fd57d564ec081811b
muthuselvamump <muthuselvamump@gmail.com>
2025-10-07 22:59:14 +0530

    MinimumAdd_to_Make_ParenthesesValid

--- MinimumAdd_to_Make_ParenthesesValid.java
+++ MinimumAdd_to_Make_ParenthesesValid.java
@@ -1,4 +1,23 @@
+class Solution921{
+    public int minAddToMakeValid(String s) {
+        Stack<Character> st=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch ==')'){
+                if(!st.isEmpty()&& st.peek()=='('){
+                    st.pop();
+                }
+                else{
+                    st.push(ch);
+                }
 
+            }
+            else{
+            st.push(ch);}
+        }
+        return st.size();
+        
+    }
+}
 public class MinimumAdd_to_Make_ParenthesesValid{
     public static void main(String []args){
 
