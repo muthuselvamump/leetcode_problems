9197a7e17380d3c7d1273dfeab0efbe3be2a6eee
muthuselvamump <muthuselvamump@gmail.com>
2025-10-08 23:23:08 +0530

    Binary_Tree_Level_Order_Traversal

--- Binary_Tree_Level_Order_Traversal.java
+++ Binary_Tree_Level_Order_Traversal.java
@@ -1,3 +1,28 @@
+class Solution101 {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans=new ArrayList<>();
+        Queue<TreeNode> q=new LinkedList<>(); 
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+            List<Integer> l=new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode temp=q.poll();
+                if(temp!=null){
+                l.add(temp.val);
+                q.offer(temp.left);
+                q.offer(temp.right);
+                
+                }
+            }
+            if(l.size()!=0){
+            ans.add(l);}
+
+        }
+
+        return ans;
+    }
+}
 public class Binary_Tree_Level_Order_Traversal {
     public static void main(String []args){
 
