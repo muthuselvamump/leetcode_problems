3707899675de465b080c56cee1e13ea4f2290a0e
muthuselvamump <muthuselvamump@gmail.com>
2025-10-14 20:27:37 +0530

    path_exit_binary_from_root_to_leaf_node

--- path_exit_binary_from_root_to_leaf_node.java
+++ path_exit_binary_from_root_to_leaf_node.java
@@ -1,3 +1,5 @@
+import javax.swing.tree.TreeNode;
+
 public class path_exit_binary_from_root_to_leaf_node {
     public static void main(String args[]){
 
@@ -6,12 +8,16 @@ public class path_exit_binary_from_root_to_leaf_node {
         if(root==null){
           return false;
         }
-        if(root.val==arr[arr.length-1] &&  root.left==null && root.right ==null ){
-            return true;
-        }
+        if (index >= arr.length) {
+        return false;
+    }
         if(root.val!=arr[index]){
             return false;
         }
+        if(root.val==arr[index] &&  root.left==null && root.right ==null && index==arr.length-1){
+            return true;
+        }
+        
 
         boolean b1=pathexit(root.left,arr,index+1);
         boolean b2=pathexit(root.right,arr,index+1);
