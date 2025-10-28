657ec08deeb6b374be2cc5d06a9a8ddf0adb5ed3
muthuselvamump <muthuselvamump@gmail.com>
2025-10-28 21:34:21 +0530

    path_sum_counts

--- path_sum_counts.java
+++ path_sum_counts.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class path_sum_counts {
@@ -21,6 +22,30 @@ public class path_sum_counts {
                                                                                                     
         count+=pathsum(root.left, list);
         count+=pathsum(root.right, list);
+        list.remove(list.size()-1);
+        return count;
+
+    }
+     public static int addingpathsum(TreeNode root,List<Integer> list,int sum,List<List<Integer>> totallist){
+        if(root==null){
+            return 0;
+        }
+        list.add(root.val);
+        int count=0;
+        int s=0;
+
+        for(int i=list.size()-1;i>=0;i--){
+            s+=list.get(i);
+            if(s==sum){
+                List<Integer> l=new ArrayList<>(list.subList(i,list.size()));
+                totallist.add(l);
+
+                count++;
+            }
+        }
+                                                                                                    
+        count+=addingpathsum(root.left, list,totallist);
+        count+=addingpathsum(root.right, list,totallist);
         list.remove(list.size()-1);
         return count;
 
