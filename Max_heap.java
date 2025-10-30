e090660256f7f95662aafa80e6ee36b20c20a5e2
muthuselvamump <muthuselvamump@gmail.com>
2025-10-30 23:13:18 +0530

    Max_heap

--- Max_heap.java
+++ Max_heap.java
@@ -22,6 +22,39 @@ class max{
 
         System.out.println(heap1.toString());
     }
+    public int remove() throws Exception{
+        if(heap1.size()==0){
+            throw new Exception("heap is empty");
+        }
+        if(heap1.size()==1){
+            return heap1.remove(0);
+        }
+        int val=heap1.get(0);
+        int last=heap1.remove(heap1.size()-1);
+        heap1.set(0,last);
+        int index=0;
+        while(index<heap1.size()){
+            int left=(index*2)+1;
+            int right=(index*2)+2;
+            int max=index;
+
+            if(left<heap1.size() && heap1.get(left)>heap1.get(max)){
+                max=left;
+            }
+            if(right<heap1.size() && heap1.get(right)>heap1.get(max)){
+                max=right;
+            }
+            if(max!=index){
+                swap(max,index);
+                index=max;
+            }
+            else{
+                break;
+            }
+
+        }
+        return val;
+    }
 }
 public class Max_heap {
     public static void main(String[] args) {
@@ -31,6 +64,13 @@ public class Max_heap {
         m.insert(22);
         m.insert(9);
         m.insert(88);
+        m.display();
+        try {
+            m.remove();
+            m.remove();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         m.display();
 
 
