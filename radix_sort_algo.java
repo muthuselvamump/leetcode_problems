53676cdbb85c3a7ee5b401ad1731fe6f0f54359f
muthuselvamump <muthuselvamump@gmail.com>
2025-11-06 19:40:22 +0530

    radix_sort_algo

--- radix_sort_algo.java
+++ radix_sort_algo.java
@@ -9,11 +9,11 @@ public class radix_sort_algo{
                 max=arr[i];
             }
         }
-        int exponent=1;
-        for(int i=0;i<max/exponent;exponent*=10){
+        for(int exponent=1;0<max/exponent;exponent*=10){
             arr=count_sort(arr,exponent);
 
         }
+        System.out.println(Arrays.toString(arr));
     }
     public static int[] count_sort(int[] arr,int exp){
         int[] count=new int[10];
@@ -22,17 +22,15 @@ public class radix_sort_algo{
         for(int i=0;i<arr.length;i++){
             count[arr[i]/exp%10]++;
         }
-        System.out.println(Arrays.toString(count));
+
         for(int i=1;i<count.length;i++){
             count[i]=count[i]+count[i-1];
         }
-        System.out.println(Arrays.toString(count));
+
         for(int i=arr.length-1;i>=0;i--){
             ans[count[arr[i]/exp%10]-1]=arr[i];
             count[arr[i]/exp%10]--;
         }
-        System.out.println(Arrays.toString(ans));
-        System.out.println();
 
         return ans;
 
